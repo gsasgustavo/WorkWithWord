30730c595095a9eba79dc339a2b198075873962f
Gustavo Siqueira <gustavo@tins.com.br>
2022-04-08 23:04:04 -0300

    Modos de Busca (Exato e Relativo).

--- Components/search_data.py
+++ Components/search_data.py
@@ -4,7 +4,7 @@ from os import path
 from unidecode import unidecode
 
 
-def search(name):
+def search(name, mode):
     """
     :param name: string
     :return: list of string
@@ -14,9 +14,14 @@ def search(name):
             raise FileNotFoundError('Arquivo para pesquisa está ausente')
         result = list()
         with open(file=r'./Assets/names.txt', mode='r', encoding='utf8') as file:
-            for line in file:
-                if name in unidecode(line.lower()):
-                    result.append(line.replace('\n', ''))
+            if mode == 1:
+                for line in file:
+                    if name in line:
+                        result.append(line.replace('\n', ''))
+            elif mode == 2:
+                for line in file:
+                    if name in unidecode(line.lower()):
+                        result.append(line.replace('\n', ''))
             return result
     except Exception as e:
         print(e.args)
--- main.py
+++ main.py
@@ -30,15 +30,23 @@ if entry == '1':
         )
         if entry not in entries:
             print('Opção inválida!\n')
-    if entry == 1:
+    if entry == '1':
         insert = insert_file(names=input(str('Insira o caminho do arquivo de Texto:\n\t')))
         if insert:
             pass
     elif entry == '2':
         insert = insert_text(names=input(str('Insira a lista de nomes:\n\t')))
 elif entry == '2':
-    text = str(input('Digite o nome a pesquisar:\n\t'))
-    result = search(name=unidecode(text.lower()))
-    print('A pesquisa por "{}" retornou {} nomes:'.format(text, len(result)))
+    name = str(input('Digite o nome a pesquisar:\n\t'))
+    mode = str(
+        input(
+            'Escolha o modo de pesquisa:\n'
+            '\t1 - Busca exata ao texto digitado\n'
+            '\t2 - Busca relativa\n\t'
+        )
+    )
+    name = unidecode(name.lower()) if mode == 2 else name
+    result = search(name=unidecode(name.lower()), mode=mode)
+    print('A pesquisa por "{}" retornou {} nomes:'.format(name, len(result)))
     for i in result:
         print(i)
